04e3d3943f3cb0589893a9c2f87199744d9d6f64
tranminhhieu <tranhieu200304@gmail.com>
2025-04-23 19:06:15 +0700

    Create header & scss

--- fe/happyshop/src/pages/user/theme/header/index.js
+++ fe/happyshop/src/pages/user/theme/header/index.js
@@ -1,10 +1,11 @@
 import { memo } from 'react'
+import './style.scss'
 
 const Header = () => {
   return (
-    <>
-      <h1>Header</h1>
-    </>
+    <div className="header_top">
+      <div className="container">Header</div>
+    </div>
   )
 }
 
