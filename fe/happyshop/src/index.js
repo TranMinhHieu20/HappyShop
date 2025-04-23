4a543fbc9231cdd787ad95886a516b6beafd36e4
tranminhhieu <tranhieu200304@gmail.com>
2025-04-23 15:01:48 +0700

    config reactjs

--- fe/happyshop/src/index.js
+++ fe/happyshop/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import HomePage from './pages/user/homePage'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<h1>123123</h1>)
+root.render(<HomePage />)
