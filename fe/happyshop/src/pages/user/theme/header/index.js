b79443e9cc9c3f78ac03fc3ba53f9cef3d18b3cf
tranminhhieu <tranhieu200304@gmail.com>
2025-04-27 04:34:06 +0700

    Create header b5

--- fe/happyshop/src/pages/user/theme/header/index.js
+++ fe/happyshop/src/pages/user/theme/header/index.js
@@ -1,10 +1,60 @@
 import { memo } from 'react'
 import './style.scss'
+import { BsFacebook } from 'react-icons/bs'
+import { BsInstagram } from 'react-icons/bs'
+import { BsLinkedin } from 'react-icons/bs'
+import { BsGlobe2 } from 'react-icons/bs'
+import { AiOutlineLogin } from 'react-icons/ai'
+import { AiTwotoneMail } from 'react-icons/ai'
+import { Link } from 'react-router-dom'
+import { formatter } from 'utils/formatter'
 
 const Header = () => {
   return (
     <div className="header_top">
-      <div className="container">Header</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-6 header__top_left">
+            <ul>
+              <li>
+                <AiTwotoneMail />
+                hello@gmail.com
+              </li>
+              <li>Miễn phí ship đơn từ {formatter(200000)}</li>
+            </ul>
+          </div>
+          <div className="col-6 header__top_right">
+            <ul className="">
+              <li>
+                <Link to={''}>
+                  <BsFacebook />
+                </Link>
+              </li>
+              <li>
+                <Link to={''}>
+                  <BsInstagram />
+                </Link>
+              </li>
+              <li>
+                <Link to={''}>
+                  <BsLinkedin />
+                </Link>
+              </li>
+              <li>
+                <Link to={''}>
+                  <BsGlobe2 />
+                </Link>
+              </li>
+              <li>
+                <Link to={''}>
+                  <AiOutlineLogin />
+                </Link>
+                <span>Đăng nhập</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
