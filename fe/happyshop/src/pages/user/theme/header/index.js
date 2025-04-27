0a44b40c6ab8ca1e6736b0d830e911ddd7514d45
tranminhhieu <tranhieu200304@gmail.com>
2025-04-27 23:45:25 +0700

    Responsive b6

--- fe/happyshop/src/pages/user/theme/header/index.js
+++ fe/happyshop/src/pages/user/theme/header/index.js
@@ -11,51 +11,60 @@ import { formatter } from 'utils/formatter'
 
 const Header = () => {
   return (
-    <div className="header_top">
-      <div className="container">
-        <div className="row">
-          <div className="col-6 header__top_left">
-            <ul>
-              <li>
-                <AiTwotoneMail />
-                hello@gmail.com
-              </li>
-              <li>Miễn phí ship đơn từ {formatter(200000)}</li>
-            </ul>
-          </div>
-          <div className="col-6 header__top_right">
-            <ul className="">
-              <li>
-                <Link to={''}>
-                  <BsFacebook />
-                </Link>
-              </li>
-              <li>
-                <Link to={''}>
-                  <BsInstagram />
-                </Link>
-              </li>
-              <li>
-                <Link to={''}>
-                  <BsLinkedin />
-                </Link>
-              </li>
-              <li>
-                <Link to={''}>
-                  <BsGlobe2 />
-                </Link>
-              </li>
-              <li>
-                <Link to={''}>
-                  <AiOutlineLogin />
-                </Link>
-                <span>Đăng nhập</span>
-              </li>
-            </ul>
+    <>
+      <div className="header_top">
+        <div className="container">
+          <div className="row">
+            <div className="col-6 header__top_left">
+              <ul>
+                <li>
+                  <AiTwotoneMail />
+                  hello@gmail.com
+                </li>
+                <li>Miễn phí ship đơn từ {formatter(200000)}</li>
+              </ul>
+            </div>
+            <div className="col-6 header__top_right">
+              <ul className="">
+                <li>
+                  <Link to={''}>
+                    <BsFacebook />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={''}>
+                    <BsInstagram />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={''}>
+                    <BsLinkedin />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={''}>
+                    <BsGlobe2 />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={''}>
+                    <AiOutlineLogin />
+                  </Link>
+                  <span>Đăng nhập</span>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-3">LOGO</div>
+          <div className="col-xl-6">MENUS</div>
+          <div className="col-xl-3">PHONE</div>
+        </div>
+      </div>
+    </>
   )
 }
 
