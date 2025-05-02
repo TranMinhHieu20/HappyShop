af8c87c8367cf79538b75b22ea2b45d6ddc4990b
tranminhhieu <tranhieu200304@gmail.com>
2025-05-03 03:04:25 +0700

    create NavHeader & Footer

--- fe/happyshop/src/pages/user/theme/footer/index.js
+++ fe/happyshop/src/pages/user/theme/footer/index.js
@@ -1,9 +1,91 @@
 import { memo } from 'react'
+import { Link } from 'react-router-dom'
+import { BsFacebook } from 'react-icons/bs'
+import { BsInstagram } from 'react-icons/bs'
+import { BsLinkedin } from 'react-icons/bs'
+import { BsGlobe2 } from 'react-icons/bs'
+import { AiOutlineLogin } from 'react-icons/ai'
+import { AiTwotoneMail } from 'react-icons/ai'
+import { formatter } from 'utils/formatter'
+import { BsCart3 } from 'react-icons/bs'
+import { ROUTERS } from 'utils/router'
+
+import './style.scss'
 
 const Footer = () => {
   return (
     <>
-      <h1>Footer</h1>
+      <footer className="footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-3">
+              <div className="footer__about">
+                <h1 className="footer__about__logo">HappyShop</h1>
+                <ul>
+                  <li>Địa chỉ: Số 8, ngõ 2 Phạm Thận Duật</li>
+                  <li>Số điện thoại: 0398-935-034</li>
+                  <li>Email: tranhieu200304@gmail.com</li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="footer__widget">
+                <h6>Cửa hàng</h6>
+                <ul>
+                  <li>
+                    <Link to="">Liên hệ</Link>
+                  </li>
+                  <li>
+                    <Link to="">Thông tin liên lạc về chúng tôi</Link>
+                  </li>
+                  <li>
+                    <Link to="">Sản phẩm kinh doanh</Link>
+                  </li>
+                </ul>
+                <ul>
+                  <li>
+                    <Link to="">Thông tin tài khoản</Link>
+                  </li>
+                  <li>
+                    <Link to="">Giỏ hàng</Link>
+                  </li>
+                  <li>
+                    <Link to="">Danh sách yêu thích</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-lg-3">
+              <div className="footer__widget">
+                <h6>Khuyến mại & ưu đãi</h6>
+                <p>Đăng ký nhận thông tin tại đây</p>
+                <form action="#">
+                  <div>
+                    <input type="text" placeholder="Nhập email"></input>
+                    <button type="submit" className="button-submit">
+                      Submit
+                    </button>
+                  </div>
+                  <div className="footer__widget__social">
+                    <div>
+                      <BsFacebook />
+                    </div>
+                    <div>
+                      <BsInstagram />
+                    </div>
+                    <div>
+                      <BsLinkedin />
+                    </div>
+                    <div>
+                      <BsGlobe2 />
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
