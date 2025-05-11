64a1e76afe9724f5fcdd393b303e7fce32d2bfc0
tranminhhieu <tranhieu200304@gmail.com>
2025-05-11 17:27:55 +0700

    create slider react

--- fe/happyshop/src/pages/user/homePage/index.js
+++ fe/happyshop/src/pages/user/homePage/index.js
@@ -1,9 +1,56 @@
 import { memo } from 'react'
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+import './style.scss'
+import cat1 from '../../../assets/user/images/categories/cat1.jpg'
+import cat2 from '../../../assets/user/images/categories/cat2.jpg'
+import cat3 from '../../../assets/user/images/categories/cat3.jpg'
+import cat4 from '../../../assets/user/images/categories/cat4.jpg'
 
 const HomePage = () => {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  }
+  const sliderItem = [
+    {
+      bgImg: cat1
+    },
+    {
+      bgImg: cat2
+    },
+    {
+      bgImg: cat3
+    },
+    {
+      bgImg: cat4
+    }
+  ]
   return (
     <>
-      <h1>HomePage</h1>
+      {/*CateGories Begin */}
+      <div className="container container__categories_slider">
+        <Carousel responsive={responsive} className="categories_slider">
+          {sliderItem.map((item, index) => (
+            <div key={index} className="categories_slider_item" style={{ backgroundImage: `url(${item.bgImg})` }}></div>
+          ))}
+        </Carousel>
+      </div>
+      {/* CateGories End */}
     </>
   )
 }
