ce5c4999462cb7b4d423382fb701245bde1a8f26
tranminhhieu <tranhieu200304@gmail.com>
2025-05-12 19:41:01 +0700

    create items products

--- fe/happyshop/src/pages/user/homePage/index.js
+++ fe/happyshop/src/pages/user/homePage/index.js
@@ -3,9 +3,13 @@ import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css'
 import './style.scss'
 import cat1 from '../../../assets/user/images/categories/cat1.jpg'
+//eslint-disable-next-line
 import cat2 from '../../../assets/user/images/categories/cat2.jpg'
 import cat3 from '../../../assets/user/images/categories/cat3.jpg'
 import cat4 from '../../../assets/user/images/categories/cat4.jpg'
+import thitbo from '../../../assets/user/images/feature/thitbo.jpg'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
+import 'react-tabs/style/react-tabs.css'
 
 const HomePage = () => {
   const responsive = {
@@ -31,15 +35,68 @@ const HomePage = () => {
       bgImg: cat1
     },
     {
-      bgImg: cat2
-    },
-    {
       bgImg: cat3
     },
     {
       bgImg: cat4
     }
   ]
+  const featProducts = {
+    all: {
+      title: 'Toàn bộ',
+      products: [
+        {
+          img: thitbo,
+          name: 'Thịt bò nạt',
+          price: 20000
+        },
+        {
+          img: thitbo,
+          name: 'Chuối',
+          price: 20000
+        }
+      ]
+    },
+    fesh: {
+      title: 'Thịt tươi',
+      products: [
+        {
+          img: thitbo,
+          name: 'Thịt tươi',
+          price: 20000
+        }
+      ]
+    }
+  }
+  const renderFeaturedProduct = (data) => {
+    const tabList = []
+    const tabPanels = []
+
+    Object.keys(data).forEach((key, index) => {
+      tabList.push(<Tab key={index}>{data[key].title}</Tab>)
+      const tabPanel = []
+      data[key].products.forEach((item, key) => {
+        tabPanel.push(
+          <>
+            <div key={key}>{item.name}</div>
+          </>
+        )
+      })
+      tabPanels.push(tabPanel)
+    })
+
+    return (
+      <Tabs>
+        <TabList>{tabList}</TabList>
+
+        {tabPanels.map((item, key) => (
+          <TabPanel key={key}>
+            <div className="row">{item}</div>
+          </TabPanel>
+        ))}
+      </Tabs>
+    )
+  }
   return (
     <>
       {/*CateGories Begin */}
@@ -51,6 +108,16 @@ const HomePage = () => {
         </Carousel>
       </div>
       {/* CateGories End */}
+      {/* Featured Begin */}
+      <div className="container">
+        <div className="featured">
+          <div className="section-title">
+            <h2>Sản phẩm nổi bật</h2>
+          </div>
+          {renderFeaturedProduct(featProducts)}
+        </div>
+      </div>
+      {/* Featured End */}
     </>
   )
 }
